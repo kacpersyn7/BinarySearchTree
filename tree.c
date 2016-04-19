92f500ea08a3908f9e660d29bd9de1a80c45ab39
kacper <kacper@kacper-Latitude-E6220>
2016-04-19 21:46:39 +0200

    second commit

---
+++ tree.c
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "tree.h"
+void delAll(node_t * root)  
+{
+    node_t * temp;
+    if (root)
+    {
+        delAll(root->left);
+        delAll(root->right);
+        temp = root;
+        free(temp);
+        root = NULL;
+    }
+}
+int insertElement(node_t *root,data_t *newelement)
+{
+    int checkadd=1;
+    int error = 0;
+    node_t *temp = root;
+    while(checkadd)
+    {
+        
+        if(strcoll((temp->book).secondname_id,newelement->secondname_id)>=0)
+        {
+            if(temp->left == NULL)
+            {
+                temp->left = (node_t*)malloc(sizeof(node_t));
+                if(temp->left != NULL)
+                {
+                    temp->left->left = NULL;
+                    temp->left->right = NULL;
+                    checkadd = 0;
+                    temp->left->book = *newelement;
+                }
+                else 
+                {
+                    delAll(root);
+                    printf("FAIL!");
+                    error = 1;
+                    break;
+                }
+            }
+            else 
+                temp = temp->left;
+        }
+        else
+        {
+           if(temp->right == NULL)
+            {
+                temp->right = (node_t*)malloc(sizeof(node_t));
+                if(temp->right != NULL)
+                {
+                    temp->right->left = NULL;
+                    temp->right->right = NULL;
+                    checkadd = 0;
+                    temp->right->book = *newelement;
+                }
+                else 
+                {
+                    delAll(root);
+                    printf("FAIL!");
+                    error = 1;
+                    break;
+                }
+            }
+           else
+               temp = temp->right;
+        }
+    }
+    return error;
+}
+void loadData(data_t *element)
+{
+    printf("\nPodaj imie\n");
+    scanf("%s", element->name);
+    printf("Podaj nazwisko\n");
+    scanf("%s", element->secondname_id);
+    printf("Podaj pierwszy numer\n");
+    scanf("%d", &(element->numberfirst));
+    printf("Podaj drugi numer\n");
+    scanf("%d", &(element->numbersecond));
+}
+void writeElement(data_t *element)
+{
+    printf("\n Imie: %s",element->name);
+    printf("\n Nazwisko: %s",element->secondname_id);
+    printf("\n Pierwszy numer: %d",element->numberfirst);
+    printf("\n Drugi numer: %d",element->numbersecond);
+}
+void writeTree(node_t *root)
+{
+    if(root != NULL)
+    {
+        writeTree(root->left);
+        writeElement(&(root->book));
+        writeTree(root->right);
+    }
+}
---
+++ tree.h
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   tree.h
+ * Author: kacper
+ *
+ * Created on 27 marca 2016, 20:26
+ */
+
+#ifndef TREE_H
+#define TREE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+typedef struct data{
+    char name[20];
+    char secondname_id[20];
+    int numberfirst;
+    int numbersecond;
+} data_t;
+typedef struct node{
+    data_t book;
+    //struct node *parent;
+    struct node *left;
+    struct node *right;
+}node_t;
+int insertElement(node_t *root,data_t *newelement);
+void loadData(data_t *);
+void writeElement(data_t *);
+void delAll(node_t *);
+void writeTree(node_t *);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TREE_H */
+
